609eff29b40b8e225ab262a8d3de5342224dc3a8
PmpPiyas <princemahmudpiyas@gmail.com>
2025-08-08 17:43:44 +0600

    Auth Last

---
+++ src/app/Modules/driver/driver.router.ts
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import { DriverControllers } from "./driver.controller";
+import { checkAuth } from "../../middleware/checkAuth";
+import { Role } from "../user/user.interfaces";
+import { validateRequest } from "../../middleware/validateRequest";
+import { driverExtensionSchema } from "./driver.validation";
+
+const router = Router();
+
+router.post(
+  "/register",
+  checkAuth(Role.RIDER, Role.DRIVER),
+  validateRequest(driverExtensionSchema),
+  DriverControllers.createDriver
+);
+
+router.get(
+  "/all-driver-request",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  DriverControllers.allDriverRequest
+);
+
+router.patch(
+  "/request-handle/:id",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  DriverControllers.driverApproveHandle
+);
+
+router.get(
+  "/",
+  checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  DriverControllers.allDrivers
+);
+
+router.get("/earnings", checkAuth(Role.DRIVER), DriverControllers.getMyEarn);
+router.get(
+  "/ride-history",
+  checkAuth(Role.DRIVER),
+  DriverControllers.getMyRideReq
+);
+
+export const DriverRoutes = router;
